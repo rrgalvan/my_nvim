4b323692f9deaaeed1e437094b112cddc5731424
Rafa Rguez Galván <rafael.rodriguez@uca.es>
2025-01-11 11:42:38 +0100

    Added neorg plugin

---
+++ lua/plugins/neorg.lua
@@ -0,0 +1,22 @@
+return {
+  'nvim-neorg/neorg',
+  lazy = false, -- Disable lazy loading as some `lazy.nvim` distributions set `lazy = true` by default
+  version = '*', -- Pin Neorg to the latest stable release
+  config = true,
+  ft = 'norg',
+  config = function()
+    require('neorg').setup {
+      load = {
+        ['core.defaults'] = {}, -- Loads default behaviour
+        ['core.concealer'] = {}, -- Adds pretty icons to your documents
+        ['core.dirman'] = { -- Manages Neorg workspaces
+          config = {
+            workspaces = {
+              notes = '~/Documentos/notas',
+            },
+          },
+        },
+      },
+    }
+  end,
+}
